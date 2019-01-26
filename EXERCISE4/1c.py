dc5d78fd7c886179fc117a113f748f12275ab511
BASTIAN JOSE <bastianjose.k@hotmail.com>
2019-01-26 13:54:39 +0530

    1a,1b,1c,1d

---
+++ EXERCISE4/1c.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import cv2
+
+from pylab import *
+import math
+
+img=cv2.imread('q1.jpg',0)
+print(img)
+HAB=np.zeros(img.shape,dtype=float64)
+
+N=img.shape[0]
+n=int (math.log(N,2))
+
+Z=np.zeros(N,dtype=float64)
+
+for i in range (N):
+	Z[i]=i/N
+
+
+K=np.zeros((N,2),dtype=float64)
+K[0][0]=0
+K[0][1]=0
+K[1][0]=0
+K[1][1]=1
+
+for i in range (1,n):
+	for j in range (1,(2**i)+1):
+		k=(2**i)+j-1
+		K[k][0]=i
+		K[k][1]=j
+
+for i in range (N):
+	for j in range (N):
+		if i==0:
+			HAB[i][j]=1
+		else:
+			p=(K[i][1]-1)/(2**K[i][0])
+			q=(K[i][1]-(1/2))/(2**K[i][0])
+			r=(K[i][1])/(2**K[i][0])
+			if p<=Z[j]<q:
+				HAB[i][j]=2**(K[i][0]/2)
+			elif q<=Z[j]<r:
+				HAB[i][j]=-2**(K[i][0]/2)
+			else:		
+				HAB[i][j]=0
+
+		
+
+HAB=HAB/(N**(1/2))			
+	
+print (HAB)
+
+HAT=np.dot(np.dot(HAB,img),np.transpose(HAB))
+print(HAT)
+recons=np.dot(np.dot(np.transpose(HAB),HAT),HAB)
+print(recons)
+cv2.imwrite("1c_basis.jpg",HAB*255)
+cv2.imwrite("1c.jpg",HAT)
+cv2.imwrite("1c_reconstruction.jpg",recons)
---
+++ EXERCISE4/1d.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import cv2
+
+from pylab import *
+
+img=cv2.imread('q1.jpg',0)
+print(img)
+CB=np.zeros(img.shape,dtype=float64)
+
+n=img.shape[0]
+for u in range(n):
+	for x in range(n):
+		if u==0:
+			CB[u][x]=np.sqrt(1/n)*(np.cos((2*x+1)*np.pi*u/(2*n)))
+		else:
+			CB[u][x]=np.sqrt(2/n)*(np.cos((2*x+1)*np.pi*u/(2*n)))
+print (CB)
+
+DCT=np.dot(np.dot(CB,img),np.transpose(CB))
+print(DCT)
+recons=np.dot(np.dot(np.transpose(CB),DCT),CB)
+print(recons)
+cv2.imwrite("1d_basis.jpg",CB*255)
+cv2.imwrite("1d.jpg",DCT)
+cv2.imwrite("1d_reconstruction.jpg",recons)
